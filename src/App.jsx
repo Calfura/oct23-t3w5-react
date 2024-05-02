4468a2030b86f8405dfb8c93ead3dc60686fca43
Calfura <travis_wilson999@hotmail.com>
2024-05-02 16:46:04 +1000

    feat: render a list of movies

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+let favouriteMovies = [
+  "Aliens",
+  "Dune",
+  "Hot Fuzz",
+  "Anchorman",
+  "Lock Stock and 2 Smoking Barrels",
+  "Shrek",
+  "Pokemon",
+  "Blazing Saddles",
+  "Snatch",
+  "Shrek 2",
+  "The Good the Bad and the Ugly",
+  "Robo Cop",
+  "Blade Runner",
+  "Lord of the Rings 1-3"
+];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Hello world!</h1>
+          <h1>Favourite Movies:</h1>
+          <ul>
+          {favouriteMovies.map((movie, index) => {
+            return <li key={movie + "-" + index}>
+              {movie}
+            </li>
+          }
+        )}
+          </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
